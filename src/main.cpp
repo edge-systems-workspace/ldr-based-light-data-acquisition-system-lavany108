91c90c61253307d361a693a9c9af19835919d26a
lavany108 <lavanya.gla_cs.aiml24@gla.ac.in>
2026-02-28 22:07:15 +0530

    added code in main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,1 +1,19 @@
 #include <Arduino.h>
+#define ldrPin A0
+void setup() {
+    // write your initialization code here
+    Serial.begin(9600);
+    pinMode(ldrPin,INPUT);
+    Serial.println("LDR Test");
+}
+void loop() {
+    // write your code here
+    int ldrRaw= analogRead(ldrPin);
+    //Map to percentage
+    int lightPercentage= map(ldrRaw,0,1023,0,100);
+    Serial.print("LDR Raw: ");
+    Serial.println(ldrRaw);
+    Serial.print("Light Percentage: ");
+    Serial.println(lightPercentage);
+    delay(500);
+}
